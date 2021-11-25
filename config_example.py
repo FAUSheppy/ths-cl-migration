050ca29a493b456ded69ef91bd2742d2ebda543d
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-11-25 17:44:16 +0100

    add SEND_NOTIFICATION to sample config

--- config_example.py
+++ config_example.py
@@ -10,6 +10,7 @@ SMB_SHARE  = "SHARE"
 DOC_TEMPLATE_PATH = "document-templates"
 
 # inbuildt notifications support https://github.com/FAUSheppy/signal-http-gateway/
+SEND_NOTIFICATION   = False
 NOTIFICATION_URL    = "URL"
 NOTIFICATION_USERS  = ["USER_1", "USER_2"]
 NOTIFICATION_AUTH_USER = "AUTH_USER"

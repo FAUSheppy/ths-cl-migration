2ec99894a659de67b71e6d30be954f690cadea4d
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2022-01-27 16:23:21 +0100

    add lower case varriations for location & lfn

--- constants.py
+++ constants.py
@@ -13,13 +13,15 @@ COLS_TO_DISPLAY_NAME = {    "projectId"  : "Project Id",
                             "adresse_FA" : "Adresse FA",
                             "adresse_fa" : "Adresse FA",
                             "ort_FA"     : "Ort FA",
+                            "ort_fa"     : "Ort FA",
                             "tel_1"      : "Festnetz",
                             "mobil"      : "Mobil",
                             "fax"        : "Fax",
                             "auftragsort": "Auftragsort",
                             "auftragsdatum" : "Auftragsdatum",
                             "lfn"        : "LFN",
-                            "laufNr"     : "Lauf Nr."
+                            "laufNr"     : "Lauf Nr.",
+                            "laufnr"     : "Lauf Nr."
                 }
 
 IS_INT_TYPE  = ["laufNr", "projectId", "PLZ_FA", "lfn", "plz_fa"]

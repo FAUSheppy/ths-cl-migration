364290a30475d66921070a70df6c5a2a276f4911
Yannik Schmidt <sheppy@atlantishq.de>
2023-03-13 16:47:23 +0100

    change: make monitoring container run in loop

--- docker/docker-monitoring/monitoring_http.py
+++ docker/docker-monitoring/monitoring_http.py
@@ -5,53 +5,58 @@ import requests
 import os
 import datetime
 import config
+import time
 
 if __name__ == "__main__":
 
-    url               = config.URL
-    monitoring_server = config.MONITORING_SERVER
-    monitoring_token  = config.MONITORING_TOKEN
-    service_name      = config.SERVICE_NAME
-    project_id        = config.PROJECT_ID or 0
-    timeout           = config.MONITORING_TIMEOUT or 10
+    while(True):
 
-    main      = requests.get("{}/".format(url), timeout=timeout)
-    entry     = requests.get("{}/entry-content?projectId={}".format(url, project_id), timeout=timeout)
-    documents = requests.get("{}/new-document?projectId={}".format(url, project_id), timeout=timeout)
-    downloadFormat = "{}/new-document?projectId={}&template=BD_Leckortung.docx"
-    documentDownload = requests.head(downloadFormat.format(url, project_id), timeout=timeout)
-    
-    error = ""
-    try:
-        main.raise_for_status()
-    except requests.HTTPError as e:
-        error += "Error: Main Page - {}\n".format(e.response.status_code)
+        url               = config.URL
+        monitoring_server = config.MONITORING_SERVER
+        monitoring_token  = config.MONITORING_TOKEN
+        service_name      = config.SERVICE_NAME
+        project_id        = config.PROJECT_ID or 0
+        timeout           = config.MONITORING_TIMEOUT or 10
 
-    try:
-        entry.raise_for_status()
-    except requests.HTTPError as e:
-        error += "Error: Entry Info Page - {}\n".format(e.response.status_code)
+        main      = requests.get("{}/".format(url), timeout=timeout)
+        entry     = requests.get("{}/entry-content?projectId={}".format(url, project_id), timeout=timeout)
+        documents = requests.get("{}/new-document?projectId={}".format(url, project_id), timeout=timeout)
+        downloadFormat = "{}/new-document?projectId={}&template=BD_Leckortung.docx"
+        documentDownload = requests.head(downloadFormat.format(url, project_id), timeout=timeout)
+        
+        error = ""
+        try:
+            main.raise_for_status()
+        except requests.HTTPError as e:
+            error += "Error: Main Page - {}\n".format(e.response.status_code)
 
-    try:
-        documents.raise_for_status()
-    except requests.HTTPError as e:
-        error += "Error: Documents Page - {}\n".format(e.response.status_code)
+        try:
+            entry.raise_for_status()
+        except requests.HTTPError as e:
+            error += "Error: Entry Info Page - {}\n".format(e.response.status_code)
 
-    try:
-        documentDownload.raise_for_status()
-    except requests.HTTPError as e:
-        error += "Error: Document Download - {}\n".format(e.response.status_code)
+        try:
+            documents.raise_for_status()
+        except requests.HTTPError as e:
+            error += "Error: Documents Page - {}\n".format(e.response.status_code)
 
-    r = requests.post(monitoring_server, 
-                        json={"service" : service_name,
-                              "token" : monitoring_token,
-                              "status" : status,
-                              "info" : "\n" + error.strip("\n")})
+        try:
+            documentDownload.raise_for_status()
+        except requests.HTTPError as e:
+            error += "Error: Document Download - {}\n".format(e.response.status_code)
 
-    status = "UNKNOWN"
-    if error:
-        status = "CRITICAL"
-    else:
-        status = "OK"
+        status = "UNKNOWN"
+        if error:
+            status = "CRITICAL"
+        else:
+            status = "OK"
 
-    r.raise_for_status()
+        r = requests.post(monitoring_server, 
+                            json={"service" : service_name,
+                                  "token" : monitoring_token,
+                                  "status" : status,
+                                  "info" : "\n" + error.strip("\n")})
+
+        r.raise_for_status()
+
+        time.sleep(5*60)

99df793ae99af4a82c50e03c922033e4660155c7
Yannik Schmidt <sheppy@atlantishq.de>
2025-03-02 18:45:39 +0100

    fix: URL parsing edge-cases & irfan path

--- windows-scripts/local_run.py
+++ windows-scripts/local_run.py
@@ -6,20 +6,28 @@ import urllib.parse
 
 WORD_PATH="C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.exe"
 PDF="C:\\Program Files\\Adobe\\Acrobat DC\\Acrobat\\Acrobat.exe"
-PICTURE="C:\\Program Files (x86)\\IrfanView\\i_view32.exe"
+PICTURE="C:\\Program Files\\IrfanView\\i_view64.exe"
 
 if __name__ == "__main__":
     
     script   = sys.argv[0]
-    file, id = urllib.parse.unquote(sys.argv[1]).split("/?")
+    print(sys.argv[1])
+    splitter = "?"
+    if "/?" in sys.argv[1]:
+        splitter = "/?"
+
+    file, id = urllib.parse.unquote(sys.argv[1]).split(splitter)
     print(file)
     file = file.lstrip("localfile://")
+
+    if file[1] != ":":
+        file = file[0] + ":" + file[1:]
 
     app    = ""
     auth = ""
     with open(os.path.join(os.path.dirname(script), "id.txt")) as f:
         auth = f.read().strip()
-    
+
     if not auth == id:
         input("Auth Missmatch")
         sys.exit(1)
@@ -32,7 +40,7 @@ if __name__ == "__main__":
     else:
         input("Bad Datatype")
         sys.exit(1)
-    
+
     cmd = [app, file]
     print(cmd)
     subprocess.Popen(cmd)
